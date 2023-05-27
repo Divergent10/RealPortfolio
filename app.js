df9f4b4a13648bef131208bab86d54d9c3a4d370
Divergent10 <lavnewboy@gmail.com>
2023-05-27 22:58:23 +0800

    final version of the desktop

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ window.onscroll = function() {scrollFunction()}
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navbar").style.border.radius = "200px";
-        document.getElementById("navbar").style.border = "1px solid white"
+        document.getElementById("navbar").style.border = "none";
+        document.getElementById("navbar").style.backgroundColor =  "#908a7623";
     }else{
         document.getElementById("navbar").style.border.radius = "none";
         document.getElementById("navbar").style.border = "none";
+        document.getElementById("navbar").style.backgroundColor = "#070831";
     }  
 }
